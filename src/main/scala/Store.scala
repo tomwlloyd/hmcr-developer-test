5d312b1ed198e7e637da1c023cc62cdbf438327a
Tom Lloyd <tom.w.lloyd@googlemail.com>
2022-08-18 20:16:16 +0100

    Step 2: Add comment describing improvements around hardcoded products, prices, & offers

--- src/main/scala/Store.scala
+++ src/main/scala/Store.scala
@@ -1,3 +1,10 @@
+/*
+This is a 'simplest possible solution' given the specific product, cost & offer requirements. In reality,
+we would not want to hardcode these, as they would most likely be database driven. The responsibility of calculating individual prices
+and discounts could be pulled into their own classes, and passed to the store on instantiation. This would allow us to wire in calls to a database in production,
+but also allow for fixed values in tests, removing the current issue where tests fail as prices and offers are updated!
+*/
+
 class Store {
   def cost(basket: List[String]): Int = {
     basket.map(cost).sum - discounts(basket)
@@ -7,14 +14,14 @@ class Store {
     case "Apple" => 60
     case "Orange" => 25
   }
-  
+
   def discounts(basket: List[String]): Int = {
     val totalApples = basket.count(_ == "Apple")
     val totalOranges = basket.count(_ == "Orange")
-    
+
     val freeApples = totalApples / 2
     val freeOranges = totalOranges / 3
-    
+
     freeApples * cost("Apple") + freeOranges * cost("Orange")
   }
 }

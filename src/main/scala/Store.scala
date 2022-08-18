c1646697f3a0eb713dbbd1f987e73805a75f7da8
Tom Lloyd <tom.w.lloyd@googlemail.com>
2022-08-18 19:46:54 +0100

    Step 2: Add discounts

--- src/main/scala/Store.scala
+++ src/main/scala/Store.scala
@@ -1,10 +1,20 @@
 class Store {
   def cost(basket: List[String]): Int = {
-    basket.map(cost).sum
+    basket.map(cost).sum - discounts(basket)
   }
 
   def cost(item: String): Int = item match {
     case "Apple" => 60
     case "Orange" => 25
+  }
+  
+  def discounts(basket: List[String]): Int = {
+    val totalApples = basket.count(_ == "Apple")
+    val totalOranges = basket.count(_ == "Orange")
+    
+    val freeApples = totalApples / 2
+    val freeOranges = totalOranges / 3
+    
+    freeApples * cost("Apple") + freeOranges * cost("Orange")
   }
 }
--- src/test/scala/StoreSuite.scala
+++ src/test/scala/StoreSuite.scala
@@ -7,12 +7,20 @@ class StoreSuite extends munit.FunSuite {
     assertBasketTotal(List("Apple"), 60)
   }
 
+  test("returns 180 for 5 Apples (2 free Apples)") {
+    assertBasketTotal(List.fill(5)("Apple"), 180)
+  }
+
   test("returns 25 for single Orange") {
     assertBasketTotal(List("Orange"), 25)
   }
 
-  test("returns 305 for 3 Apples and 5 Oranges") {
-    assertBasketTotal(List("Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Orange"), 305)
+  test("returns 75 for 4 Oranges (1 free Orange)") {
+    assertBasketTotal(List.fill(4)("Orange"), 75)
+  }
+
+  test("returns 220 for 3 Apples and 5 Oranges") {
+    assertBasketTotal(List.fill(3)("Apple") :++ List.fill(5)("Orange"), 220)
   }
 
   def assertBasketTotal(basket: List[String], expectedTotal: Int): Unit = {
